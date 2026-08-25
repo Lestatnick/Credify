40501c2798cc1144012feabb3e0482cab3b1cd34
GABRIEL JOSÉ DA SILVA MACIEL <gabriel.maciel6174201@edu.pe.senac.br>
2026-08-25 20:53:41 -0300

    ajustes

---
+++ scr/Validacao.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class AulaPedro {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+        System.out.println("Digite o valor do veículo: R$  ");
+        Double valor_veiculo = scanner.nextDouble();
+        System.out.println("Digite seu salário mensal: ");
+        Double salario_mensal = scanner.nextDouble();
+        System.out.println("Digite a quanntidade de parcelas: (12x a 60x )");
+        int quantidade_parcelas = scanner.nextInt();
+
+        Double valor_parcelas = valor_veiculo / quantidade_parcelas
+
+        Double limite_parcela = valor_veiculo * 0.30;
+
+        if (quantidade_parcelas != 60 && quantidade_parcelas != 12 && quantidade_parcelas != 24 && quantidade_parcelas != 36 &&
+                quantidade_parcelas != 48) {
+            System.out.println("Erro, Quantidade inválida ");
+        } else if ( > limite_parcela);
+        System.out.println("Valor acima do orçamento! ");
+    }
+
+}
+
+
+
+
+
+
+
+
