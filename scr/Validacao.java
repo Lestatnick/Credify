920baa13c5bc89916c1d8aad0a742962fb01617c
GABRIEL JOSÉ DA SILVA MACIEL <gabriel.maciel6174201@edu.pe.senac.br>
2026-08-25 21:07:50 -0300

    Ajuste ortográficos nos terminais e adição de comentários para a melhora na compreensão do código

--- scr/Validacao.java
+++ scr/Validacao.java
@@ -2,19 +2,20 @@ import java.util.Scanner;
 public class AulaPedro {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        // inputs e entrada de dados
+
         System.out.println("Digite seu nome: ");
         String nome = scanner.nextLine();
         System.out.println("Digite o valor do veículo: R$  ");
         Double valor_veiculo = scanner.nextDouble();
-        System.out.println("Digite seu salário mensal: ");
+        System.out.println("Digite o salário mensal: ");
         Double salario_mensal = scanner.nextDouble();
-        System.out.println("Digite a quanntidade de parcelas: (12x a 60x )");
+        System.out.println("Digite a quantidade de parcelas: (12x a 60x )");
         int quantidade_parcelas = scanner.nextInt();
-
-        Double valor_parcelas = valor_veiculo / quantidade_parcelas
 
         Double limite_parcela = valor_veiculo * 0.30;
 
+        // Condicionais  e validações/notificações de erro
         if (quantidade_parcelas != 60 && quantidade_parcelas != 12 && quantidade_parcelas != 24 && quantidade_parcelas != 36 &&
                 quantidade_parcelas != 48) {
             System.out.println("Erro, Quantidade inválida ");
